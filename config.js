0041c7333229a1f43b215b3fe8039eef6c2a7d5c
aleckurser <aleckurser@gmail.com>
2025-08-08 03:57:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG, || "",
+ALIVE_IMG: process.env.ALIVE_IMG, || "https://files.catbox.moe/mwf1ib.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG, || "Hello,I'm Alive now",
 };
